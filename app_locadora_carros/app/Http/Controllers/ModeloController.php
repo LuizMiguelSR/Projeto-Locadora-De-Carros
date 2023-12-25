dcd075d8136406e1b3c3cf0effcbe77281db3cac
LuizMiguelSR <luizmiguel.srodrigues@gmail.com>
2023-12-25 13:18:41 -0300

    Update: Refactoring do endpoint update de marca e modelo

--- app_locadora_carros/app/Http/Controllers/ModeloController.php
+++ app_locadora_carros/app/Http/Controllers/ModeloController.php
@@ -105,7 +105,11 @@ class ModeloController extends Controller
         $imagem = $request->file('imagem');
         $imagem_urn = $imagem->store('imagens/modelos', 'public');
 
-        $modelo->update([
+        $modelo->fill($request->all());
+        $modelo->imagem = $imagem_urn;
+        $modelo->save();
+
+        /* $modelo->update([
             'modelo_id' => $request->modelo_id,
             'nome' => $request->nome,
             'imagem' => $imagem_urn,
@@ -113,7 +117,7 @@ class ModeloController extends Controller
             'lugares' => $request->lugares,
             'air_bag' => $request->air_bag,
             'abs' => $request->abs,
-        ]);
+        ]); */
 
         return response()->json($modelo, 200);
     }
